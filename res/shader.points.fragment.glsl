86fa5fd667e740472616ffa501d328e9d5213d43
Marco Neumann <marco@crepererum.net>
2016-05-05 10:23:17 +0200

    save (hidden) sqrt-operation in fragment shader

--- res/shader.points.fragment.glsl
+++ res/shader.points.fragment.glsl
@@ -7,7 +7,14 @@ out vec4 color;
 uniform float inv_n;
 
 void main() {
-    float dcenter = distance(vec2(0.5, 0.5), gl_PointCoord);
-    float fade = 1.0 - step(0.5, dcenter);
+    vec2  delta    = vec2(0.5, 0.5) - gl_PointCoord;
+    float dcenter2 = dot(delta, delta);
+
+    // discard with some offset (for smoothing)
+    if (dcenter2 > 0.26) {
+        discard;
+    }
+
+    float fade = 1.0 - step(0.25, dcenter2);
     color = vec4(pointcolor, 1.0) * fade * vec4(inv_n);
 }
